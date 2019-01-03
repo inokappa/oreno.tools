2bfde8de3f45c920b2b79b25c159782396d99b84
kappa <kappa@KappaBook-Pro.local>
2019-01-03 11:20:43 +0900

    Add Circle CI

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 bundle exec jekyll build
-aws s3 sync ./_site/ s3://oreno.tools/ \
-   --exclude "jekyll-cayman-theme.gemspec" \
-   --exclude "README.md" \
-   --exclude "deploy.sh" \
-   --exclude "*releases/*"
+# aws s3 sync ./_site/ s3://oreno.tools/ \
+#    --exclude "jekyll-cayman-theme.gemspec" \
+#    --exclude "README.md" \
+#    --exclude "deploy.sh" \
+#    --exclude "*releases/*"
